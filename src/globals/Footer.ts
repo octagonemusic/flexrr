c0d20b7bd713028698cfcdc16f88e55c72075fd3
octagonemusic <octagonemusic69@gmail.com>
2025-02-25 20:06:32 +0530

    Added globals (Header & Footer)

---
+++ src/globals/Footer.ts
@@ -0,0 +1,38 @@
+import type { GlobalConfig } from 'payload'
+
+export const Header: GlobalConfig = {
+  slug: 'footer',
+  fields: [
+    {
+      name: 'logo',
+      label: 'Logo',
+      type: 'upload',
+      relationTo: 'media',
+      required: true,
+    },
+    {
+      name: 'nav',
+      label: 'Navigation',
+      type: 'array',
+      fields: [
+        {
+          name: 'label',
+          label: 'Label',
+          type: 'text',
+        },
+        {
+          name: 'link',
+          label: 'Link',
+          type: 'text',
+        },
+      ],
+      minRows: 1,
+      maxRows: 5,
+    },
+    {
+      name: 'copyrightNotice',
+      label: 'CopyRight Notice',
+      type: 'text',
+    },
+  ],
+}
